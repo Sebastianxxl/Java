cbf96b088946527ea12cf1264e22cdaecfe0e1e6
Grupa 2 Java <seinho_>
2018-11-09 18:01:08 +0200

    import

---
+++ src/Data.java
@@ -0,0 +1,17 @@
+public class Data {
+    public static void main(String[] args) {
+        int myAge;
+        myAge = 32;
+        float weight = 60.5f;
+        double height = 356.6;
+        long distance = 10023215225522l;
+        char gender = 'm';
+        char test = 9;
+        boolean isStudent = false;
+        boolean hasJob = true;
+        int test2 = (int) weight;
+        System.out.print(test);
+        System.out.print(test2);
+    }
+}
+
