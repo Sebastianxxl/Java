2ab84ece13e42fb376b9f35f3b49b19ec2573918
Grupa 2 Java <seinho_>
2018-11-13 20:00:31 +0200

    import

---
+++ src/Robot.java
@@ -0,0 +1,28 @@
+import stanford.karel.Karel;
+
+public class Robot extends Karel {
+   public void run() {
+       move();
+       move();
+       turnLeft();
+       turnLeft();
+       turnLeft();
+       move();
+       turnLeft();
+       move();
+       pickBeeper();
+       turnLeft();
+       turnLeft();
+       move();
+       move();
+       move();
+       turnLeft();
+       turnLeft();
+       turnLeft();
+       move();
+       putBeeper();
+       turnLeft();
+       turnLeft();
+       turnLeft();
+   }
+}
